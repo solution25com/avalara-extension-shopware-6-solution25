46bd24c35621a4221d04f01959ae49170652760a
solution-25 <info@solution25.com>
2025-05-06 13:52:46 +0200

    Avalara fix for nonTaxable proruct

--- src/Core/Checkout/Cart/AvalaraPriceProcessorDecorate.php
+++ src/Core/Checkout/Cart/AvalaraPriceProcessorDecorate.php
@@ -8,17 +8,21 @@ use Shopware\Core\Checkout\Cart\Cart;
 use Shopware\Core\Checkout\Cart\CartBehavior;
 use Shopware\Core\Checkout\Cart\LineItem\CartDataCollection;
 use Shopware\Core\Checkout\Cart\LineItem\LineItem;
+use Shopware\Core\Checkout\Cart\Order\OrderConverter;
 use Shopware\Core\Checkout\Cart\Price\QuantityPriceCalculator;
 use Shopware\Core\Checkout\Cart\Price\Struct\CalculatedPrice;
 use Shopware\Core\Checkout\Cart\Tax\Struct\CalculatedTax;
 use Shopware\Core\Checkout\Cart\Tax\Struct\CalculatedTaxCollection;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Symfony\Component\HttpFoundation\Session\Session;
 use MoptAvalara6\Adapter\AvalaraSDKAdapter;
 use Monolog\Logger;
 use MoptAvalara6\Core\Checkout\Cart\OverwritePriceProcessor;
+use Shopware\Core\Checkout\Cart\Tax\Struct\TaxRule;
+use Shopware\Core\Checkout\Cart\Tax\Struct\TaxRuleCollection;
 
 class AvalaraPriceProcessorDecorate extends OverwritePriceProcessor
 {
@@ -33,19 +37,24 @@ class AvalaraPriceProcessorDecorate extends OverwritePriceProcessor
 
   private $avalaraTaxes;
 
+  private EntityRepository $orderLineItemRepository;
+
   private Logger $logger;
 
 
   public function __construct(
     QuantityPriceCalculator $calculator,
-    SystemConfigService $systemConfigService,
-    EntityRepository $categoryRepository,
-    Logger $loggerMonolog,
-  ) {
+    SystemConfigService     $systemConfigService,
+    EntityRepository        $categoryRepository,
+    EntityRepository        $orderLineItemRepository,
+    Logger                  $loggerMonolog,
+  )
+  {
     $this->calculator = $calculator;
     $this->systemConfigService = $systemConfigService;
     $this->session = new SessionService();
     $this->categoryRepository = $categoryRepository;
+    $this->orderLineItemRepository = $orderLineItemRepository;
     $this->logger = $loggerMonolog;
   }
 
@@ -56,7 +65,9 @@ class AvalaraPriceProcessorDecorate extends OverwritePriceProcessor
     $adapter = new AvalaraSDKAdapter($this->systemConfigService, $this->logger, $salesChannelId);
     $this->avalaraTaxes = $this->session->getValue(Form::SESSION_AVALARA_TAXES_TRANSFORMED, $adapter);
 
-    if ($this->isTaxesUpdateNeeded()) {
+
+    if ($this->isTaxesUpdateNeeded() && $original->getDeliveries()->getAddresses()->getCountries()->first()) {
+
       $service = $adapter->getService('GetTax');
       $this->avalaraTaxes = $service->getAvalaraTaxes($original, $context, $this->session, $this->categoryRepository);
 
@@ -65,8 +76,7 @@ class AvalaraPriceProcessorDecorate extends OverwritePriceProcessor
 
     if ($this->avalaraTaxes
       && array_key_exists(Form::TAX_REQUEST_STATUS, $this->avalaraTaxes)
-      && $this->avalaraTaxes[Form::TAX_REQUEST_STATUS] == Form::TAX_REQUEST_STATUS_SUCCESS)
-    {
+      && $this->avalaraTaxes[Form::TAX_REQUEST_STATUS] == Form::TAX_REQUEST_STATUS_SUCCESS) {
       $this->changeTaxes($toCalculate);
       $this->changeShippingCosts($toCalculate);
       $this->changePromotionsTaxes($toCalculate);
@@ -89,6 +99,7 @@ class AvalaraPriceProcessorDecorate extends OverwritePriceProcessor
       }
 
       $originalPrice = $product->getPrice();
+
 
       $avalaraProductPriceCalculated = $this->itemPriceCalculator($originalPrice, $productNumber);
 
@@ -147,6 +158,15 @@ class AvalaraPriceProcessorDecorate extends OverwritePriceProcessor
       $price->getTotalPrice()
     );
 
+    if($this->avalaraTaxes[$productNumber]['tax'] == 0) {
+      $this->avalaraTaxes[$productNumber]['rate'] = 0;
+    }
+
+    //For TaxRules
+    $taxRules[] = new TaxRule($this->avalaraTaxes[$productNumber]['rate']);
+
+    $taxRuleCollection = new TaxRuleCollection(array_merge($price->getTaxRules()->getElements(), $taxRules));
+
     $avalaraCalculatedTaxCollection = new CalculatedTaxCollection();
     $avalaraCalculatedTaxCollection->add($avalaraCalculatedTax);
 
@@ -154,7 +174,7 @@ class AvalaraPriceProcessorDecorate extends OverwritePriceProcessor
       $price->getUnitPrice(),
       $price->getTotalPrice(),
       $avalaraCalculatedTaxCollection,
-      $price->getTaxRules(),
+      $taxRuleCollection,
       $price->getQuantity(),
       $price->getReferencePrice(),
       $price->getListPrice()
@@ -197,8 +217,11 @@ class AvalaraPriceProcessorDecorate extends OverwritePriceProcessor
       'capture',
       'google-capture',
       'apple-capture',
-      'api/_proxy-order/'
+      'api/_proxy-order/',
+      'api/_action/order/',
+      '/return'
     ];
+
 
     $currentPage = $_SERVER['REQUEST_URI'];
 
